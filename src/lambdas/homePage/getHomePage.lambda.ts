dfdebd1bc99877513d94bb1386dd06fcfbccb4b4
MOSCOW\Mikhail_Semichev <Mikhail_Semichev@epam.com>
2022-09-16 17:14:20 +0300

    aaaa

--- src/lambdas/homePage/getHomePage.lambda.ts
+++ src/lambdas/homePage/getHomePage.lambda.ts
@@ -1,21 +1,24 @@
 import { AppSyncResolverEvent, AppSyncResolverHandler } from 'aws-lambda';
-// import {
-//   MessageQueryFarewellMessageArgs,
-//   MessageQueryWelcomeMessageArgs,
-// } from '../generated/resolvers-types';
 import { HomePage } from './homePage';
+import * as cs from 'contentstack';
 
-// type QueryArgs = MessageQueryArgs;
-// type MessageQueryArgs = MessageQueryWelcomeMessageArgs | MessageQueryFarewellMessageArgs;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-//export const getHeader: AppSyncResolverHandler<QueryArgs, Header> = async (event) => {
+// Initialize the Contentstack Stack
+const CS_API_KEY = process.env.CS_API_KEY || 'blt0619d4d4d9959be5';
+const CS_DELIVERY_TOKEN = process.env.CS_API_KEY || 'csda4a97f7107c3d0476a31a20';
+const CS_ENVIRONMENT = process.env.CS_ENVIRONMENT || 'ms-test';
+const Stack = cs.Stack(CS_API_KEY, CS_DELIVERY_TOKEN, CS_ENVIRONMENT);
 
 export const getHomePage: AppSyncResolverHandler<unknown, HomePage> = async (event, context) => {
   const header: HomePage = {
     title: 'Calvin Home',
     image: '/img1.png',
   };
+
+  const Query = Stack.ContentType('ms_webpage').Entry('blte47750c9395a38e5');
+  const entry = await Query.fetch();
+
+  header.image = entry.get('image') || 'empty';
+  header.title = entry.get('title');
 
   return header;
 };
